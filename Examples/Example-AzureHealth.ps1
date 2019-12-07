d0cbaa13c012233539afa20a1609e20f813fb6e5
Przemysław Kłys <przemyslaw.klys@evotec.pl>
2019-12-07 22:23:53 +0100

    1st commit

---
+++ Examples/Example-AzureHealth.ps1
@@ -0,0 +1,20 @@
+﻿Import-Module .\PSWinDocumentation.AzureHealthService.psd1 -Force
+
+$Azure = Get-WinAzureHealth -Formatted
+$Azure
+
+New-HTML {
+    foreach ($Region in $Azure.Keys) {
+        New-HTMLTab -Name $Region {
+
+            New-HTMLTable -DataTable $Azure.$Region -Filtering {
+                foreach ($Column in $Azure.$Region[0].PSObject.Properties.Name) {
+                    New-HTMLTableCondition -Name $Column -Value 'Good' -BackGroundColor Green -Color White -Alignment center
+                    New-HTMLTableCondition -Name $Column -Value 'Information' -BackGroundColor Blue -Color White -Alignment center
+                    New-HTMLTableCondition -Name $Column -Value 'Warning' -BackGroundColor Orange -Alignment center
+                    New-HTMLTableCondition -Name $Column -Value 'Critical' -BackGroundColor Red -Color White  -Alignment center
+                }
+            }
+        }
+    }
+} -FilePath $PSScriptRoot\AzureHealth.Html -UseCssLinks -UseJavaScriptLinks -TitleText 'Azure' -ShowHTML
---
+++ Public/Get-WinAzureHealth.ps1
@@ -0,0 +1,32 @@
+﻿function Get-WinAzureHealth {
+    [CmdletBinding()]
+    param(
+        [switch] $OriginalOutput,
+        [switch] $Formatted
+    )
+    $AzureStatus = ConvertFrom-HtmlTable -Url "https://status.azure.com/en-us/status" -ReplaceContent @{ 'Blank' = '' }
+    if (-not $OriginalOutput) {
+        if ($Formatted) {
+            $AzureOutput = [ordered] @{
+                Americas                 = $AzureStatus[0]
+                Europe                   = $AzureStatus[1]
+                'Asia Pacific'           = $AzureStatus[2]
+                'Middle East and Africa' = $AzureStatus[3]
+                'Azure Government'       = $AzureStatus[4]
+                'Azure China'            = $AzureStatus[5]
+            }
+        } else {
+            $AzureOutput = [ordered] @{
+                Americas            = $AzureStatus[0]
+                Europe              = $AzureStatus[1]
+                AsiaPacific         = $AzureStatus[2]
+                MiddleEastAndAfrica = $AzureStatus[3]
+                AzureGovernment     = $AzureStatus[4]
+                AzureChina          = $AzureStatus[5]
+            }
+        }
+        $AzureOutput
+    } else {
+        $AzureStatus
+    }
+}
